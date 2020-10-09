05c7af11c1460a5d0cae1abb174e3df381782cdb
destroyedlolo <l_faillie@yahoo.com>
2020-10-10 01:37:41 +0200

    Split low_energy() and wakeup()

--- DomoWatch/DomoWatch.ino
+++ DomoWatch/DomoWatch.ino
@@ -23,12 +23,6 @@ Created by Lewis he on October 10, 2019.
 #define G_EVENT_VBUS_REMOVE         _BV(1)
 #define G_EVENT_CHARGE_DONE         _BV(2)
 
-#define G_EVENT_WIFI_SCAN_START     _BV(3)
-#define G_EVENT_WIFI_SCAN_DONE      _BV(4)
-#define G_EVENT_WIFI_CONNECTED      _BV(5)
-#define G_EVENT_WIFI_BEGIN          _BV(6)
-#define G_EVENT_WIFI_OFF            _BV(7)
-
 enum {
     Q_EVENT_BMA_INT,
     Q_EVENT_AXP_INT,
@@ -48,199 +42,209 @@ bool lenergy = false;
 TTGOClass *ttgo;
 
 
-	/* Go to sleep */
+/*****
+ * Handle sleep
+ *****/
+
 void low_energy(){
-    if(ttgo->bl->isOn()){
-        xEventGroupSetBits(isr_group, WATCH_FLAG_SLEEP_MODE);
-        ttgo->closeBL();
-        ttgo->stopLvglTick();
-        ttgo->bma->enableStepCountInterrupt(false);
-        ttgo->displaySleep();
+/* Enter in low energy */
+
+	xEventGroupSetBits(isr_group, WATCH_FLAG_SLEEP_MODE);
+	ttgo->closeBL();
+	ttgo->stopLvglTick();
+	ttgo->bma->enableStepCountInterrupt(false);
+	ttgo->displaySleep();
 
 #if 0		/* pas logic */
-        if(!WiFi.isConnected()){
-            lenergy = true;
-            WiFi.mode(WIFI_OFF);
-            // rtc_clk_cpu_freq_set(RTC_CPU_FREQ_2M);
-            setCpuFrequencyMhz(20);
+		if(!WiFi.isConnected()){
+			lenergy = true;
+			WiFi.mode(WIFI_OFF);
+			// rtc_clk_cpu_freq_set(RTC_CPU_FREQ_2M);
+			setCpuFrequencyMhz(20);
 
-            Serial.println("ENTER IN LIGHT SLEEEP MODE");
-            gpio_wakeup_enable ((gpio_num_t)AXP202_INT, GPIO_INTR_LOW_LEVEL);
-            gpio_wakeup_enable ((gpio_num_t)BMA423_INT1, GPIO_INTR_HIGH_LEVEL);
-            esp_sleep_enable_gpio_wakeup ();
-            esp_light_sleep_start();
-        }
+			Serial.println("ENTER IN LIGHT SLEEEP MODE");
+			gpio_wakeup_enable ((gpio_num_t)AXP202_INT, GPIO_INTR_LOW_LEVEL);
+			gpio_wakeup_enable ((gpio_num_t)BMA423_INT1, GPIO_INTR_HIGH_LEVEL);
+			esp_sleep_enable_gpio_wakeup ();
+			esp_light_sleep_start();
+		}
 #endif
-    } else {
-        ttgo->startLvglTick();
-        ttgo->displayWakeup();
-        ttgo->rtc->syncToSystem();
-        updateStepCounter(ttgo->bma->getCounter());
-        updateBatteryLevel();
-        updateBatteryIcon(LV_ICON_CALCULATION);
-        lv_disp_trig_activity(NULL);
-        ttgo->openBL();
-        ttgo->bma->enableStepCountInterrupt();
-    }
+}
+
+void wakeup(){
+/* wake up from low energy mode */
+
+	ttgo->startLvglTick();
+	ttgo->displayWakeup();
+	ttgo->rtc->syncToSystem();
+	updateStepCounter(ttgo->bma->getCounter());
+	updateBatteryLevel();
+	updateBatteryIcon(LV_ICON_CALCULATION);
+	lv_disp_trig_activity(NULL);
+	ttgo->openBL();
+	ttgo->bma->enableStepCountInterrupt();
 }
 
 void setup(){
-    Serial.begin(2000000);
+	Serial.begin(1152000);
 
-    //Create a program that allows the required message objects and group flags
-    g_event_queue_handle = xQueueCreate(20, sizeof(uint8_t));
-    g_event_group = xEventGroupCreate();
-    isr_group = xEventGroupCreate();
+	//Create a program that allows the required message objects and group flags
+	g_event_queue_handle = xQueueCreate(20, sizeof(uint8_t));
+	g_event_group = xEventGroupCreate();
+	isr_group = xEventGroupCreate();
 
-    //Initialize TWatch
-    ttgo = TTGOClass::getWatch();	
-    ttgo->begin();
+	//Initialize TWatch
+	ttgo = TTGOClass::getWatch();	
+	ttgo->begin();
 
-    // Turn on the IRQ used
-    ttgo->power->adc1Enable(AXP202_BATT_VOL_ADC1 | AXP202_BATT_CUR_ADC1 | AXP202_VBUS_VOL_ADC1 | AXP202_VBUS_CUR_ADC1, AXP202_ON);
-    ttgo->power->enableIRQ(AXP202_VBUS_REMOVED_IRQ | AXP202_VBUS_CONNECT_IRQ | AXP202_CHARGING_FINISHED_IRQ, AXP202_ON);
-    ttgo->power->clearIRQ();
+	// Turn on the IRQ used
+	ttgo->power->adc1Enable(AXP202_BATT_VOL_ADC1 | AXP202_BATT_CUR_ADC1 | AXP202_VBUS_VOL_ADC1 | AXP202_VBUS_CUR_ADC1, AXP202_ON);
+	ttgo->power->enableIRQ(AXP202_VBUS_REMOVED_IRQ | AXP202_VBUS_CONNECT_IRQ | AXP202_CHARGING_FINISHED_IRQ, AXP202_ON);
+	ttgo->power->clearIRQ();
 
-    // Turn off unused power
-    ttgo->power->setPowerOutPut(AXP202_EXTEN, AXP202_OFF);
-    ttgo->power->setPowerOutPut(AXP202_DCDC2, AXP202_OFF);
-    ttgo->power->setPowerOutPut(AXP202_LDO3, AXP202_OFF);
-    ttgo->power->setPowerOutPut(AXP202_LDO4, AXP202_OFF);
+	// Turn off unused power
+	ttgo->power->setPowerOutPut(AXP202_EXTEN, AXP202_OFF);
+	ttgo->power->setPowerOutPut(AXP202_DCDC2, AXP202_OFF);
+	ttgo->power->setPowerOutPut(AXP202_LDO3, AXP202_OFF);
+	ttgo->power->setPowerOutPut(AXP202_LDO4, AXP202_OFF);
 
-    //Initialize lvgl
-    ttgo->lvgl_begin();
+	//Initialize lvgl
+	ttgo->lvgl_begin();
 
-    // Enable BMA423 interrupt 
-    // The default interrupt configuration,
-    // you need to set the acceleration parameters, please refer to the BMA423_Accel example
-    ttgo->bma->attachInterrupt();
+	// Enable BMA423 interrupt 
+	// The default interrupt configuration,
+	// you need to set the acceleration parameters, please refer to the BMA423_Accel example
+	ttgo->bma->attachInterrupt();
 
-    //Connection interrupted to the specified pin
-    pinMode(BMA423_INT1, INPUT);
-    attachInterrupt(BMA423_INT1, [] {
-        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-        EventBits_t  bits = xEventGroupGetBitsFromISR(isr_group);
-        if(bits & WATCH_FLAG_SLEEP_MODE){ //! For quick wake up, use the group flag
-            xEventGroupSetBitsFromISR(isr_group, WATCH_FLAG_SLEEP_EXIT | WATCH_FLAG_BMA_IRQ, &xHigherPriorityTaskWoken);
-        } else {
-            uint8_t data = Q_EVENT_BMA_INT;
-            xQueueSendFromISR(g_event_queue_handle, &data, &xHigherPriorityTaskWoken);
-        }
+	//Connection interrupted to the specified pin
+	pinMode(BMA423_INT1, INPUT);
+	attachInterrupt(BMA423_INT1, [] {
+		BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+		EventBits_t  bits = xEventGroupGetBitsFromISR(isr_group);
+		if(bits & WATCH_FLAG_SLEEP_MODE){ //! For quick wake up, use the group flag
+			xEventGroupSetBitsFromISR(isr_group, WATCH_FLAG_SLEEP_EXIT | WATCH_FLAG_BMA_IRQ, &xHigherPriorityTaskWoken);
+		} else {
+			uint8_t data = Q_EVENT_BMA_INT;
+			xQueueSendFromISR(g_event_queue_handle, &data, &xHigherPriorityTaskWoken);
+		}
 
-        if(xHigherPriorityTaskWoken){
-            portYIELD_FROM_ISR();
-        }
-    }, RISING);
+		if(xHigherPriorityTaskWoken){
+			portYIELD_FROM_ISR();
+		}
+	}, RISING);
 
-    // Connection interrupted to the specified pin
-    pinMode(AXP202_INT, INPUT);
-    attachInterrupt(AXP202_INT, [] {
-        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-        EventBits_t  bits = xEventGroupGetBitsFromISR(isr_group);
-        if(bits & WATCH_FLAG_SLEEP_MODE){
-            //! For quick wake up, use the group flag
-            xEventGroupSetBitsFromISR(isr_group, WATCH_FLAG_SLEEP_EXIT | WATCH_FLAG_AXP_IRQ, &xHigherPriorityTaskWoken);
-        } else {
-            uint8_t data = Q_EVENT_AXP_INT;
-            xQueueSendFromISR(g_event_queue_handle, &data, &xHigherPriorityTaskWoken);
-        }
+	// Connection interrupted to the specified pin
+	pinMode(AXP202_INT, INPUT);
+	attachInterrupt(AXP202_INT, [] {
+		BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+		EventBits_t  bits = xEventGroupGetBitsFromISR(isr_group);
+		if(bits & WATCH_FLAG_SLEEP_MODE){
+			//! For quick wake up, use the group flag
+			xEventGroupSetBitsFromISR(isr_group, WATCH_FLAG_SLEEP_EXIT | WATCH_FLAG_AXP_IRQ, &xHigherPriorityTaskWoken);
+		} else {
+			uint8_t data = Q_EVENT_AXP_INT;
+			xQueueSendFromISR(g_event_queue_handle, &data, &xHigherPriorityTaskWoken);
+		}
 
-        if(xHigherPriorityTaskWoken){
-            portYIELD_FROM_ISR ();
-        }
-    }, FALLING);
+		if(xHigherPriorityTaskWoken){
+			portYIELD_FROM_ISR ();
+		}
+	}, FALLING);
 
-    //Check if the RTC clock matches, if not, use compile time
-    ttgo->rtc->check();
+	//Check if the RTC clock matches, if not, use compile time
+	ttgo->rtc->check();
 
-    //Synchronize time to system time
-    ttgo->rtc->syncToSystem();
+	//Synchronize time to system time
+	ttgo->rtc->syncToSystem();
 
-    //Execute your own GUI interface
-    setupGui();
+	//Execute your own GUI interface
+	setupGui();
 
-    //Clear lvgl counter
-    lv_disp_trig_activity(NULL);
+	//Clear lvgl counter
+	lv_disp_trig_activity(NULL);
 
-    //When the initialization is complete, turn on the backlight
-    ttgo->openBL();
+	//When the initialization is complete, turn on the backlight
+	ttgo->openBL();
 }
 
 void loop(){
-    bool  rlst;
-    uint8_t data;
+	bool  rlst;
+	uint8_t data;
 
-    //! Fast response wake-up interrupt
-    EventBits_t  bits = xEventGroupGetBits(isr_group);
-    if(bits & WATCH_FLAG_SLEEP_EXIT){
-        if(lenergy){
-            lenergy = false;
-            setCpuFrequencyMhz(160);
-        }
+	//! Fast response wake-up interrupt
+	EventBits_t  bits = xEventGroupGetBits(isr_group);
+	if(bits & WATCH_FLAG_SLEEP_EXIT){
+		if(lenergy){
+			lenergy = false;
+			setCpuFrequencyMhz(160);
+		}
 
-        low_energy();
+		wakeup();
 
-        if(bits & WATCH_FLAG_BMA_IRQ){
-            do {
-                rlst =  ttgo->bma->readInterrupt();
-            } while(!rlst);
-            xEventGroupClearBits(isr_group, WATCH_FLAG_BMA_IRQ);
-        }
-        if(bits & WATCH_FLAG_AXP_IRQ){
-            ttgo->power->readIRQ();
-            ttgo->power->clearIRQ();
-            //TODO: Only accept axp power pek key short press
-            xEventGroupClearBits(isr_group, WATCH_FLAG_AXP_IRQ);
-        }
-        xEventGroupClearBits(isr_group, WATCH_FLAG_SLEEP_EXIT);
-        xEventGroupClearBits(isr_group, WATCH_FLAG_SLEEP_MODE);
-    }
+		if(bits & WATCH_FLAG_BMA_IRQ){
+			do {
+				rlst =  ttgo->bma->readInterrupt();
+			} while(!rlst);
+			xEventGroupClearBits(isr_group, WATCH_FLAG_BMA_IRQ);
+		}
+		if(bits & WATCH_FLAG_AXP_IRQ){
+			ttgo->power->readIRQ();
+			ttgo->power->clearIRQ();
+				//TODO: Only accept axp power pek key short press
+			xEventGroupClearBits(isr_group, WATCH_FLAG_AXP_IRQ);
+		}
+		xEventGroupClearBits(isr_group, WATCH_FLAG_SLEEP_EXIT);
+		xEventGroupClearBits(isr_group, WATCH_FLAG_SLEEP_MODE);
+	}
 
-    if((bits & WATCH_FLAG_SLEEP_MODE)){
-        //! No event processing after entering the information screen
-        return;
-    }
+	if((bits & WATCH_FLAG_SLEEP_MODE)){
+		//! No event processing after entering the information screen
+		return;
+	}
 
-    //! Normal polling
-    if(xQueueReceive(g_event_queue_handle, &data, 5 / portTICK_RATE_MS) == pdPASS){
-        switch(data){
-        case Q_EVENT_BMA_INT:
-            do {
-                rlst =  ttgo->bma->readInterrupt();
-            } while(!rlst);
+	//! Normal polling
+	if(xQueueReceive(g_event_queue_handle, &data, 5 / portTICK_RATE_MS) == pdPASS){
+		switch(data){
+		case Q_EVENT_BMA_INT:
+			do {
+				rlst =  ttgo->bma->readInterrupt();
+			} while(!rlst);
 
-            //! step counter
-            if(ttgo->bma->isStepCounter()){
-                updateStepCounter(ttgo->bma->getCounter());
-            }
-            break;
+			//! step counter
+			if(ttgo->bma->isStepCounter()){
+				updateStepCounter(ttgo->bma->getCounter());
+			}
+			break;
 
-        case Q_EVENT_AXP_INT:
-            ttgo->power->readIRQ();
+		case Q_EVENT_AXP_INT:
+			ttgo->power->readIRQ();
 
-            if(ttgo->power->isVbusPlugInIRQ())
-                updateBatteryIcon(LV_ICON_CHARGE);
+			if(ttgo->power->isVbusPlugInIRQ())
+				updateBatteryIcon(LV_ICON_CHARGE);
 	
-            if(ttgo->power->isVbusRemoveIRQ())
-                updateBatteryIcon(LV_ICON_CALCULATION);
+			if(ttgo->power->isVbusRemoveIRQ())
+				updateBatteryIcon(LV_ICON_CALCULATION);
 
-            if(ttgo->power->isChargingDoneIRQ())
-                updateBatteryIcon(LV_ICON_CALCULATION);
+			if(ttgo->power->isChargingDoneIRQ())
+				updateBatteryIcon(LV_ICON_CALCULATION);
 
-            if(ttgo->power->isPEKShortPressIRQ()){
-                ttgo->power->clearIRQ();
-                low_energy();
-                return;
-            }
+			if(ttgo->power->isPEKShortPressIRQ()){
+				ttgo->power->clearIRQ();
+				if(ttgo->bl->isOn())
+					low_energy();
+				else
+					wakeup();
+				return;
+			}
 
-            ttgo->power->clearIRQ();
-            break;
+			ttgo->power->clearIRQ();
+			break;
 
-        }
-    }
+		}
+	}
 
-    if(lv_disp_get_inactive_time(NULL) < DEFAULT_SCREEN_TIMEOUT)
-        lv_task_handler();
-    else	// No activities : going to sleep
-        low_energy();
+	if(lv_disp_get_inactive_time(NULL) < DEFAULT_SCREEN_TIMEOUT)
+		lv_task_handler();
+	else	// No activities : going to sleep
+		low_energy();
 }
